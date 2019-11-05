42e7974a252a57fa91259395d371a9f017b94114
Alexander Kresnov <kresnov@gmail.com>
2019-11-05 18:25:12 +0300

    Решил задачи

---
+++ variables/main.js
@@ -0,0 +1,40 @@
+// Задача 1
+console.log('Задача 1');
+console.log();
+
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+
+let result = (algebra + geography + physics)/3 ;
+
+console.log(result);
+
+console.log();
+
+// Задача 2
+console.log('Задача 2');
+console.log();
+
+let myName = 'Вася';
+
+let message = 'Привет, мир! Меня зовут ' + myName + '.';
+console.log(message);
+
+console.log();
+
+// Задача 3
+console.log('Задача 3');
+console.log();
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result2 =x*y+5*z+x-1;
+
+console.log('Ответ на задачу: ' + result2);
+
+
+console.log();
+
