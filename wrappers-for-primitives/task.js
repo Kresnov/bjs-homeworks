de6957b7d9c12e9491aee8cd54be25b2352e3543
Alexander Kresnov <kresnov@gmail.com>
2019-11-13 20:12:40 +0300

    в 1 задании затык с date.getFullYear is not a func

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+"use strict"
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,9 +12,33 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    percent = Number(percent) / 100;
+    contribution = Number(contribution);
+    amount = Number(amount);
+    if (isNaN(percent)) {
+      console.log(`Не числовое значение`);
+    }
+    if (isNaN(contribution)) {
+        console.log(`Не числовое значение`);
+    }
+    if (isNaN(amount)) {
+        console.log(`Не числовое значение`);
+    }
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    let nowYear = new Date().getFullYear();
+    let nowMonth = new Date().getMonth() + 1;
+    let creditYear = date.getFullYear();
+    let creditMonth = date.getMonth() + 1;
+    let amountMonths = (creditYear - nowYear) * 12 + (creditMonth - nowMonth);
+
+
+    let allcredit = amount - contribution;
+    let sumEveryMonth = allcredit * ((percent/12) + (percent/12) / (Math.pow((1 + (percent/12)), amountMonths) - 1));
+    let totalAmount = sumEveryMonth * amountMonths;
+
+    console.log(`Общая сумма выплаты: ${totalAmount.toFixed(2)}`);
+  
+    return totalAmount.toFixed(2);
 }
 
 function sayHello() {
@@ -23,6 +49,11 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    if (name == false) {
+        name = 'Аноним';
+    }
+      
+    console.log(`Привет, мир! Меня зовут ${name}.`);
+    let greeting = `Привет, мир! Меня зовут ${name}.`;
+    return greeting;
 }
