7a51f22065c5484829fcc62fb774c6640e8c4469
Alexander Kresnov <kresnov@gmail.com>
2019-11-22 13:54:49 +0300

    Добавил проверки в console.log

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,16 +1,14 @@
 'use strict';
 
-// Задание 1
-
+// задание 1
 
 function compareArrays(arr1, arr2){
-  return (arr1.length === arr2.length) && (arr1.every(item => arr1.indexOf(item) === arr2.indexOf(item)));
+	return (arr1.length === arr2.length) && (arr1.every(item => arr1.indexOf(item) === arr2.indexOf(item)));
 }
 
 function memoize(fn, limit) {
 	const results = [];
 	return function() {
-		
 		let solution = results.find(item => compareArrays(item.args, Array.from(arguments)));
 
 		if(solution) {
@@ -19,8 +17,7 @@ function memoize(fn, limit) {
 			if(results.length === limit) {
 				results.shift();
             }
-            
-			let result = fn(...arguments);
+            let result = fn(...arguments);
 			results.push({
 				args: Array.from(arguments),
 				result: result,
@@ -28,9 +25,26 @@ function memoize(fn, limit) {
 
 			return `Новое значение - ${result}`;
 		}
+		console.log(results);
 	} 
 }
 const sum = (a, b) => a + b;
 
 const mSum = memoize(sum, 10); 
 
+console.log(mSum(1, 2));
+console.log(mSum(1, 2));
+console.log(mSum(1, 5));
+console.log(mSum(1, 9));
+console.log(mSum(1, 9));
+console.log(mSum(1, 9));
+console.log(mSum(1, 5));
+console.log(mSum(6, 7));
+console.log(mSum(5, 7));
+console.log(mSum(4, 7));
+console.log(mSum(3, 7));
+console.log(mSum(2, 7));
+console.log(mSum(2, 6));
+console.log(mSum(3, 6));
+console.log(mSum(4, 6));
+console.log(mSum(1, 2));
